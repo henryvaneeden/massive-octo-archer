2b93a652d4f1b31d3bdc524fb0708316a275a5f7
Henry van Eeden <henry@eset.co.za>
2013-10-30 16:12:48 +0200

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
 maintainer "Henry van Eeden"
 description "Will setup an nginx server for load balancing to goliath api servers"
 
-recipe "massive-octo-archer::goliath", "sets up the nginx with config to load balance goliath api servers"
+recipe "massive-octo-archer::api", "sets up the nginx with config to load balance goliath api servers"
