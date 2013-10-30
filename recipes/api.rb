40d1cdb137402d1ac984040b9c93202c6a043e11
Henry van Eeden <henry@eset.co.za>
2013-10-30 15:54:02 +0200

    added base recipe just loggin

---
+++ recipes/api.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name : massive-octo-archer
+# Recipe : api
+#
+
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+		Chef::Log.info("This is a test application #{application} as it is not an PHP repo")
+end
