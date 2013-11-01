7e551e9abb897926bb80863b72df305297a4443c
Henry van Eeden <henry@eset.co.za>
2013-11-01 12:43:14 +0200

    Test dependencies

--- ninja/metadata.rb
+++ ninja/metadata.rb
@@ -1,4 +1,7 @@
 maintainer "Henry van Eeden"
 description "Will setup an nginx server for load balancing to goliath api servers"
 
+depends "deploy"
+depends "nginx"
+
 recipe "ninja::install", "sets up the nginx with config to load balance goliath api servers"
