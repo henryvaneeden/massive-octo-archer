fbeee83684b8bff29a4b8cab0fd2da05aa33cf0c
Henry van Eeden <henry@eset.co.za>
2013-11-01 16:47:43 +0200

    deploy git

--- ninja/recipes/install.rb
+++ ninja/recipes/install.rb
@@ -6,11 +6,13 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-	Chef::Log.info("This is test 2")
-		Chef::Log.info("This is a test application #{application}")
-		Chef::Log.info("This is the deploy ibject: #{deploy.to_yaml}")
-end
+  Chef::Log.info("This is test 2")
+  Chef::Log.info("This is a test application #{application}")
+  Chef::Log.info("This is the deploy ibject: #{deploy.to_yaml}")
 
-deploy "api" do
-  Chef::Log.info("This is a test application deploy api #{application}")
+  deploy "api" do
+    repo "#{node['repository']}"
+    revision "HEAD"
+    action :deploy
+  end
 end
