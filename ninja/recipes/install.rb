02739a8d6a07f2114abffb80941c9411aeea44ab
Henry van Eeden <henry@eset.co.za>
2013-11-01 11:53:02 +0200

    arrgghhh

--- ninja/recipes/install.rb
+++ ninja/recipes/install.rb
@@ -6,6 +6,7 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
+	Chef::Log.info("This is test 2")
 		Chef::Log.info("This is a test application #{application}")
 		Chef::Log.info("This is the deploy ibject: #{deploy.to_yaml}")
 end
